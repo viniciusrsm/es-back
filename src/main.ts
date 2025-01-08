1978d39297d8dcc1c5baecfe5b783a57d4053347
IthaloDekki <ithalomedei76@gmail.com>
2025-01-08 20:19:42 -0300

    abilitando o enableCors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Habilitar CORS para permitir requisições do front-end
+  app.enableCors({
+    origin: 'http://localhost:3000', // Substitua pelo domínio do front em produção
+    credentials: true, // Se você usar cookies/sessões
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
